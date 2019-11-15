e9962ddfc83e667170380e17a1828755b8f25472
Eva Peralta <eva.peralta@gmail.com>
2019-11-15 10:09:55 +0100

    Añadida la variable d
    He añadido la variable d

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,13 +3,15 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		
-		int a,b,c;
+		int a,b,c,d;
 		a=5;
 		System.out.println(a);
 		b=7;
 		System.out.println(b);
 		c=10;
 		System.out.println(c);
+		d=8;
+		System.out.println(d);
 	}
 
 }
